09846fd7a996050b263499b0f538918217cdfb0e
Panji Rajasa <igustipanjirajasa@gmail.com>
2024-11-13 12:42:48 +0800

    complete all basic requierenment

---
+++ script/script.js
@@ -0,0 +1,25 @@
+// Untuk login
+
+/* Only for reference */
+
+// function loginUser(event) {
+//     event.preventDefault();  // Cegah pengiriman form default
+//     // Simpan status login di localStorage
+//     localStorage.setItem('isLoggedIn', 'true');
+//     // Redirect ke halaman homepage
+//     window.location.href = 'homepage.html';
+// }
+
+// // Untuk Sign up
+// // Cek status login saat halaman dimuat
+// if (localStorage.getItem('isLoggedIn') !== 'true') {
+//     // Jika belum login, redirect ke halaman login
+//     window.location.href = 'index.html';
+// }
+
+// function logoutUser() {
+//     // Hapus status login dari localStorage
+//     localStorage.removeItem('isLoggedIn');
+//     // Redirect ke halaman login
+//     window.location.href = 'index.html';
+// }
