0550f4ab66e86ab659bac681419b2e4e5f2fe093
Panji Rajasa <igustipanjirajasa@gmail.com>
2024-11-13 22:41:20 +0800

    fungsi ok, fix data XI TKR2

--- script/script.js
+++ script/script.js
@@ -23,3 +23,21 @@
 //     // Redirect ke halaman login
 //     window.location.href = 'index.html';
 // }
+
+const inputBoxes = document.querySelectorAll(".input-box");
+
+inputBoxes.forEach((input, index) => {
+  input.addEventListener("input", () => {
+    if (input.value.length === 1) {
+      if (index < inputBoxes.length - 1) {
+        inputBoxes[index + 1].focus();
+      }
+    } else if (input.value.length === 0) {
+      // Check if input is empty
+      if (index > 0) {
+        // If not the first input box
+        inputBoxes[index - 1].focus();
+      }
+    }
+  });
+});
